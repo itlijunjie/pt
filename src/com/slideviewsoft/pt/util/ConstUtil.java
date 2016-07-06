75f2153b01e528ad7bf89d0104316d9a28005f44
itlijunjie <itlijunjie@gmail.com>
2016-07-06 23:30:20 +0800

    modify 修改IP根据配置改变，免得每次都得修改IP。

--- src/com/slideviewsoft/pt/util/ConstUtil.java
+++ src/com/slideviewsoft/pt/util/ConstUtil.java
@@ -1,17 +1,34 @@
 package com.slideviewsoft.pt.util;
 
-public class ConstUtil {
-	// public static final
-	
-	public static final String IP="172.16.20.41";
-	
-	public static final String PORT="8080";
-	
-	public static final String URL_SEPARATING_CHARACTER = "http://"+IP+":"+PORT+"/pt";
-	
-	public static final String SERVER_NAME = "http://"+IP+":"+PORT+"/pt";
-	
-	public static final String SERVER_RESOURCES = "http://"+IP+":"+PORT+"/pt/resources";
-	
-	public static final String JSON_TEST_PATH = "http://"+IP+":"+PORT+"/pt/call/json";
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public final class ConstUtil {
+	public static final String IP = ConstUtil.getIP();
+
+	public static final String PORT = "8080";
+
+	public static final String URL_SEPARATING_CHARACTER = "http://" + IP + ":"
+			+ PORT + "/pt";
+
+	public static final String SERVER_NAME = "http://" + IP + ":" + PORT
+			+ "/pt";
+
+	public static final String SERVER_RESOURCES = "http://" + IP + ":" + PORT
+			+ "/pt/resources";
+
+	public static final String JSON_TEST_PATH = "http://" + IP + ":" + PORT
+			+ "/pt/call/json";
+
+	static String getIP() {
+		String res = "";
+		try {
+			InetAddress addr = InetAddress.getLocalHost();
+			res = addr.getHostAddress();
+		} catch (UnknownHostException e) {
+			System.out.println("严重错误，没有获取到本地IP");
+			e.printStackTrace();
+		}
+		return res;
+	}
 }
